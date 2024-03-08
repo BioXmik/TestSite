18b7c6573c2ab85afe82dc67b3103def12006c5e
BioXmik <77557979+BioXmik@users.noreply.github.com>
2024-03-09 00:42:58 +0300

    update

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ tg.expand();
 
 let clameButton = document.getElementById("clame-button");
 clameButton.addEventListener("click", () => {
-    alert("Привет, Skillbox")
     const data = { first_name, last_name, username } = tg.initDataUnsafe.user;
     tg.sandData(JSON.stringify(data));
     tg.close();
