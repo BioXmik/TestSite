6a724d077feb198995fb46821d549f42458c0172
BioXmik <77557979+BioXmik@users.noreply.github.com>
2024-03-09 01:10:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ tg.expand();
 
 let clameButton = document.getElementById("clame-button");
 clameButton.addEventListener("click", function() {
+    
     const data = { first_name, last_name, username } = window.Telegram.WebApp.initDataUnsafe.user;
     tg.sandData(JSON.stringify(data));
     tg.close();
