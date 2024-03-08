2a61117af7cbeb0a6d278b19b59aa8e76ac93157
BioXmik <77557979+BioXmik@users.noreply.github.com>
2024-03-08 23:24:15 +0300

    fist commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+let clameButton = document.getElementById("clame-button");
+clameButton.addEventListener("click", function() {
+    tg.sandData("clame");
+});
