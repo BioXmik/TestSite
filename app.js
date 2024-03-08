7fb3383b51720c9b759359d721802319d8a00e14
BioXmik <77557979+BioXmik@users.noreply.github.com>
2024-03-09 00:50:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ tg.expand();
 
 let clameButton = document.getElementById("clame-button");
 clameButton.addEventListener("click", () => {
+    print("ffdfgf");
     tg.sandData(JSON.stringify("fdfffdf"));
     tg.close();
 });
