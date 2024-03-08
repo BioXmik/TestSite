f35faac5f19237d70a535631911ff76bbdea9c7b
BioXmik <77557979+BioXmik@users.noreply.github.com>
2024-03-09 01:19:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,5 @@ tg.expand();
 
 let clameButton = document.getElementById("clame-button");
 clameButton.addEventListener("click", function() {
-
-    let data = {
-        name: tg.initDataUnsafe.user.username
-    }
-    tg.sandData(JSON.stringify(data));
     tg.close();
 });
