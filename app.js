2ae8864c1863b253ed4d86e47446666a03b3c700
BioXmik <77557979+BioXmik@users.noreply.github.com>
2024-03-10 18:58:18 +0300

    update

--- app.js
+++ app.js
@@ -2,8 +2,50 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+let userId = 880668193;
+
 let clameButton = document.getElementById("clame-button");
+let balanceText = document.getElementById("balance-text");
+let refLinkCopyButton = document.getElementById("ref-link-copy-button");
+
+getBalance(userId);
+
+function getBalance(userId) {
+    fetch(`http://localhost:5000/balance/${userId}`)
+        .then(response => response.text())
+        .then(balance => {
+            balanceText.textContent = balance;
+        })
+        .catch(error => {
+            // Handle error
+    });
+}
+
+function clame(userId) {
+    fetch(`http://localhost:5000/clame/${userId}`)
+        .then(response => response.text())
+        .then(balance => {
+            balanceText.textContent = balance;
+        })
+        .catch(error => {
+            // Handle error
+    });
+}
+
+function copyToClipboard(str) {
+    var area = document.createElement('textarea');
+  
+    document.body.appendChild(area);  
+    area.value = str;
+    area.select();
+    document.execCommand("copy");
+    document.body.removeChild(area);  
+}
+
 clameButton.addEventListener("click", () => {
-    tg.sandData(JSON.stringify("BioXmik"));
-    tg.close();
+    clame(userId);
+});
+
+refLinkCopyButton.addEventListener("click", () => {
+    copyToClipboard(userId);
 });
